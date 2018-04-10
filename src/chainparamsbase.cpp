f36433bc0a9c59ca029477aa37b0b82bddb14ab9
Coin Stew <info@coinstew.com>
2018-04-10 15:20:33 -0700

    updated RPC default port

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -32,7 +32,7 @@ class CBaseMainParams : public CBaseChainParams
 public:
     CBaseMainParams()
     {
-        nRPCPort = 18889;
+        nRPCPort = 36137;
     }
 };
 static CBaseMainParams mainParams;
